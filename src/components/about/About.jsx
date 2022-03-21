c0628b0bb4bd6019c408698c52d743775eb52830
mlk00 <mrcs.arts@hotmail.com>
2022-03-21 03:35:00 -0300

    about section done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__cointainer">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About ME" />           
+          </div>
+        </div>
+
+        <div className="about__cotent">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit aut consequatur esse nam. Deleniti placeat nesciunt fugit quo accusamus vel, quos voluptatem atque adipisci, inventore id, aliquam velit magni distinctio!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 

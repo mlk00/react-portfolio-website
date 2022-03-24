1c6d094ff10c140e5fbcc9b3cc1f352117629881
mlk00 <mrcs.arts@hotmail.com>
2022-03-23 22:08:00 -0300

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
-function Testimonials() {
+const  Testimonials = () => {
   return (
-    <section id='testmonials'>Testimonials</section>
+    <section id='testmonials'>
+      <h5>Review from clients</h5>3
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
